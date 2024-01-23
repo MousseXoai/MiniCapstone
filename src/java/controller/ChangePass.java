c8d807bc0a3597f4052b0eb2c6c147eebf7fc6b9
tomtom <minh942002@gmail.com>
2024-01-23 15:42:39 +0700

    update validate

--- src/java/controller/ChangePass.java
+++ src/java/controller/ChangePass.java
@@ -30,9 +30,9 @@ public class ChangePass extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
 
-        String oldPass = request.getParameter("oldPass"); // old
-        String newPass = request.getParameter("pass"); // new
-        String rePass = request.getParameter("repass");
+        String oldPass = request.getParameter("oldPass").trim(); // old
+        String newPass = request.getParameter("pass").trim(); // new
+        String rePass = request.getParameter("repass").trim();
 
         DAO cp = new DAO();
         HttpSession session = request.getSession();
@@ -51,6 +51,11 @@ public class ChangePass extends HttpServlet {
                 request.getRequestDispatcher("ChangePass.jsp").forward(request, response);
             } else if (!newPass.equals(rePass)) {
                 String errorMessage = "Mật khẩu mới không trùng nhau!";
+                request.setAttribute("errorMessage", errorMessage);
+                request.getRequestDispatcher("ChangePass.jsp").forward(request, response);
+            } else if (oldPass.isEmpty() || newPass.isEmpty() || rePass.isEmpty()
+                    || oldPass.contains(" ") || newPass.contains(" ") || rePass.contains(" "))   {
+                String errorMessage = "Các trường không được chứa khoảng trắng.";
                 request.setAttribute("errorMessage", errorMessage);
                 request.getRequestDispatcher("ChangePass.jsp").forward(request, response);
             } else {
